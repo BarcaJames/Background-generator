9002ce71817d0cc0ad687b05e9be3128c64b1aed
Jamie James <jamie1k_86@yahoo.com>
2019-04-27 10:18:56 -0500

    adding initial files

---
+++ main.js
@@ -0,0 +1,24 @@
+/**Used in browserify */
+var whatever = require('lodash');
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log('answer:', whatever.without(array, 1, 2, 3));
+console.log("Trying gitHub here");
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector("#color1");
+var color2 = document.querySelector("#color2");
+var body = document.querySelector("#body");
+
+function setGradient() {
+    body.style.background = "linear-gradient(to right, " +
+        color1.value + "," +
+        color2.value + ")";
+
+    css.innerHTML = "background: linear-gradient(to right, " +
+        color1.value + ", " + color2.value + ");";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
