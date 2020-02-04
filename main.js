9b20ad2536b227badeb45009eb3aa8cb1fb6ffde
BarcaJames <jamie1k_86@yahoo.com>
2020-02-04 12:25:42 -0500

    Removed unused packages and clean up code

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 /**Used in browserify */
-var whatever = require('lodash');
-
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log('answer:', whatever.without(array, 1, 2, 3));
-console.log("Trying gitHub here e");
 
 var css = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
@@ -11,12 +6,15 @@ var color2 = document.querySelector("#color2");
 var body = document.querySelector("#body");
 
 function setGradient() {
-    body.style.background = "linear-gradient(to right, " +
-        color1.value + "," +
-        color2.value + ")";
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
 
-    css.innerHTML = "background: linear-gradient(to right, " +
-        color1.value + ", " + color2.value + ");";
+  css.innerHTML =
+    "background: linear-gradient(to right, " +
+    color1.value +
+    ", " +
+    color2.value +
+    ");";
 }
 
 color1.addEventListener("input", setGradient);
